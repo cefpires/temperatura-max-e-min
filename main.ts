abb59a44a66896c77c350cc281dba8b1771741b6
cefpires <120255027+cefpires@users.noreply.github.com>
2022-12-19 10:23:46 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,42 @@
+let Tmax = 0
+let Tmin = 0
+let Temp = 0
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    basic.showString("T max")
+    basic.showNumber(Tmax)
+})
+input.onButtonPressed(Button.AB, function () {
+    Tmin = input.temperature()
+    Tmax = input.temperature()
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    basic.showString("T min")
+    basic.showNumber(Tmin)
+})
+basic.forever(function () {
+    if (input.temperature() > Tmax) {
+        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+        Tmax = input.temperature()
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (input.temperature() < Tmin) {
+        music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once)
+        Tmin = input.temperature()
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    Temp = input.temperature()
+})
